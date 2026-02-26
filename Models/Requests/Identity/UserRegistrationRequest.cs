ba82131fad06d45dcb2755d6ec678059cc3d1274
Thomas Testi <tmtesti@gmail.com>
2026-02-26 13:55:29 -0600

    Added user registration request model

---
+++ Models/Requests/Identity/UserRegistrationRequest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponseWrapperLib.Models.Requests.Identity
+{
+
+    public class UserRegistrationRequest
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+        public string PhoneNumber { get; set; }
+        public bool AutoConfirmEmail { get; set; }
+        public bool ActiveteUser { get; set; }
+
+
+
+    }
+}
